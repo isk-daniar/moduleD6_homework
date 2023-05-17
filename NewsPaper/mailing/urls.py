95a884310e11a89fb45a35445279719db891a7d6
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2023-05-17 11:30:20 +0500

    d6

--- NewsPaper/mailing/urls.py
+++ NewsPaper/mailing/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import MailingView
 
-app_name = 'appointments'
+app_name = 'mailing'
 
 urlpatterns = [
     path('make_mailing/', MailingView.as_view(), name="make_mailing"),
